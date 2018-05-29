f2b48d036b83ff70dc337bfae2d27b644dcd7f37
Ove <770541944@qq.com>
2018-05-30 01:11:00 +0800

    配置result worker为mongodb

---
+++ mongodb_result_worker.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+# Created on 2018-03-22
+#
+#   saving spider result into mongodb
+#
+
+from pyspider.result import ResultWorker
+from log.logger import *
+import mongodb.mongo_cli as mongo
+
+logger = getLogger("pyspider.mongodb_result_worker")
+# mongo cli
+cli = mongo.__get__()
+# mongo database
+database_name = "keepindoors"
+# mongo collection
+collection_name = "docs"
+
+class MyResultWorker(ResultWorker):
+    def on_result(self, task, result):
+        # 抓取到的数据不为空
+        assert task['taskid']
+        assert task['project']
+        assert task['url']
+        assert result['url']
+        assert result['title']
+        assert result['content']
+        assert result['date']
+        assert result['docno']
+        assert result['dir_date'],mongo
+        assert result["content"]
+        assert result["textrank"]
+        assert result["simhash"]
+
+        # save into mongodb
+        mongo.insertDoc(result,cli,database_name,collection_name)
+        logger.info("project=%s,taskid=%s,url=%s,result=%s",task['project'],task['taskid'],task['url'],result["title"])
--- spider_scripts/news.ifeng.com/processor_2.py
+++ spider_scripts/news.ifeng.com/processor_2.py
@@ -8,10 +8,12 @@
 
 from pyspider.libs.base_handler import *
 from datetime import timedelta, datetime
-import logging
+from log.logger import *
 import time
+from core.textrank import textrank
+from core.simhash import simhash
 
-logger = logging.getLogger("news.ifeng.com")
+logger = getLogger("news.ifeng.com")
 
 class Handler(BaseHandler):
     crawl_config = {
@@ -27,7 +29,7 @@ class Handler(BaseHandler):
     _page_no = 1
     _address_postfix = 'rtlist.shtml'
     # how long the day to be crawled
-    _long = 365;
+    _long = 30;
     '''
         taskid : the same with url
     '''
@@ -45,8 +47,8 @@ class Handler(BaseHandler):
             date = datetime.today() + timedelta(-i)
             date = date.strftime('%Y%m%d')
             seed_url = self._address_prefix + '/'+ date + '/' + str(self._page_no) + '/' +  self._address_postfix
-            # each spider seed wile execute after 10 second, one by one
-            self.crawl(seed_url,callback=self.next_page, validate_cert=False,exetime=time.time()+ i * 265, retries=5)
+            # each spider seed wile execute after 10 mintues, one by one
+            self.crawl(seed_url,callback=self.next_page, validate_cert=False,exetime=time.time()+ i * 600, retries=3)
             logger.info("################# on_start:" +seed_url)
 
     @config(age=-1,priority=1)
@@ -62,8 +64,8 @@ class Handler(BaseHandler):
         for e in detail_elements:
             detail_url = e.attr['href']
             if detail_url:
-                # each detail_page task wile execute after 0.01 second, one by one
-                self.crawl(detail_url,callback=self.detail_page, validate_cert=False,exetime=time.time()+ i * 0.3,retries=5)
+                # each detail_page task wile execute after 0.5 second, one by one
+                self.crawl(detail_url,callback=self.detail_page, validate_cert=False,exetime=time.time()+ i * 0.5,retries=3)
                 i = i + 1
                 logger.info("################# next_page->detail_url:" + detail_url)
 
@@ -74,7 +76,7 @@ class Handler(BaseHandler):
             next_page_url = e.attr['href']
             if e.text().lstrip() == "下一页" and next_page_url:
                 # each next_page task wile execute after 0.5 second, one by one
-                self.crawl(next_page_url,callback=self.next_page, validate_cert=False,exetime=time.time()+ i * 20,retries=5)
+                self.crawl(next_page_url,callback=self.next_page, validate_cert=False,exetime=time.time()+ i * 50,retries=3)
                 logger.info("################# next_page->next_page_url:")
                 break
 
@@ -85,14 +87,61 @@ class Handler(BaseHandler):
         '''
         # fetching date from the url  http://news.ifeng.com/listpage/11502/20180321/1/rtlist.shtml
         dir_date = task['url'][24:32]
-        return {
+        content = ""
+        for e in response.doc.items('div#main_content.js_selection_area p'):
+            c = e.text()
+            bre = False
+            # delete <p> start with "原标题:" "资料|" "校对|"
+            for s in ["原标题:","资料|","校对|"]:
+                if c.startswith(s):
+                    bre = True
+                    break
+            if bre:
+                break
+            content += e.text()
+
+        # filter rules
+        content = self.filter(content)
+
+        # raw type transfer to result type
+        raw = {
             "url": task['url'],
             "date": response.doc('[itemprop="datePublished"]').text().lstrip(),
             "title": response.doc('[itemprop="headline"]').text(),
-            "content":response.doc('#artical').text(),
+            "content":content,
             "dir_date":dir_date,
         }
+        return self.raw2result(raw)
 
     def on_finished(self, response, task):
         '''called when all tasks finished'''
         logger.info("################# on_finished:"+task['url'])
+
+
+    def filter(self,content):
+        """
+        remove the useless info of the content
+        :param content: str
+        :return: str
+            the filtered content
+        """
+        # TODO: add specified filter rules
+        return content
+
+    def raw2result(self,raw):
+        """
+        transfer from raw to result
+        :param raw: dict
+        :return: dict
+        """
+        # url to docno
+        assert raw["url"]
+        raw["docno"] = str(hash(raw["url"]))
+
+        # text rank
+        textrank([raw])
+
+        # sim hash
+        raw["simhash"] = simhash(raw["textrank"])
+
+        return raw
